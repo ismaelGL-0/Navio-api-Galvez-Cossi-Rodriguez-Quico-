ff57a338bb85db77894490bca21708a3fa296fd1
ismaelGL-0 <ismaglgit0@gmail.com>
2026-04-05 23:39:31 -0500

    first commit

---
+++ src/main/java/idat/pe/navio/NavioApplication.java
@@ -0,0 +1,13 @@
+package idat.pe.navio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NavioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NavioApplication.class, args);
+	}
+
+}
